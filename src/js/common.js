35373626ddb48b79a6a51c9417dce96a9c486895
Eugene Godun <eugene.godun@gmail.com>
2015-12-14 14:13:01 +0700

    index page html

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,42 @@
 "use strict";
 $(document).ready(function() {
-	
+	function number() { 
+		var number = $(".js-number");
+		number.each(function(){
+			var max_number = +($(this).attr("data-max-number"));
+			var input = $(this).find("input");
+			var plus = $(this).find(".js-plus-number");
+			var minus = $(this).find(".js-minus-number");
+			plus.on("click", function(){
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false
+				}
+				else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.on("click", function(){
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				return false;
+			});
+			input.on("change", function(){
+				var val = +$(this).val();
+				if (val > max_number) {
+					val = max_number;
+					$(this).val(val);
+				}
+				if (val == '') {
+					val = 1;
+					$(this).val(val);
+				}
+			});
+		});
+	}
+	number();
 });
