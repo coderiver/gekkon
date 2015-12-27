5daa8d933043d73416619410abb925f12a5179fa
Eugene Godun <eugene.godun@gmail.com>
2015-12-27 21:49:12 +0700

    personal

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 //=include lib/jquery.js
+//=include lib/jquery-ui.min.js
+//=include lib/datepicker-ru.js
 //=include lib/bootstrap.min.js
 //=include lib/slick.min.js
 //=include lib/jquery.masonry.min.js
