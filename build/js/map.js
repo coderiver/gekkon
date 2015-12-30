8cab53c52739f20f7a93b989f98a876c9c290032
Eugene Godun <eugene.godun@gmail.com>
2015-12-30 13:21:43 +0700

    map

--- build/js/map.js
+++ build/js/map.js
@@ -216,351 +216,145 @@ AmCharts.themes.light = {
     }
 };
 
-var map = AmCharts.makeChart("chartdiv", {
+var europe = "#a7c66d";
+var mapEurope = AmCharts.makeChart("chartdiv", {
 
 	"type": "map",
-  "theme": "light",
-
-    dataProvider: {
-      map: "worldLow",
-      zoomLevel: 3.5,
-      zoomLongitude: 10,
-      zoomLatitude: 52,
-      areas: [ {
-          title: "Austria",
-          id: "AT",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Ireland",
-          id: "IE",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Denmark",
-          id: "DK",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Finland",
-          id: "FI",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Sweden",
-          id: "SE",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Great Britain",
-          id: "GB",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Italy",
-          id: "IT",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "France",
-          id: "FR",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Spain",
-          id: "ES",
-          color: "#67b7dc",
-          customData: "1986"
-        }, {
-          title: "Greece",
-          id: "GR",
-          color: "#67b7dc",
-          customData: "1981"
-        }, {
-          title: "Germany",
-          id: "DE",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Belgium",
-          id: "BE",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Luxembourg",
-          id: "LU",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Netherlands",
-          id: "NL",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Portugal",
-          id: "PT",
-          color: "#67b7dc",
-          customData: "1986"
-        },
-
-        {
-          title: "Lithuania",
-          id: "LT",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Latvia",
-          id: "LV",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Czech Republic ",
-          id: "CZ",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Slovakia",
-          id: "SK",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Slovenia",
-          id: "SI",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Estonia",
-          id: "EE",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Hungary",
-          id: "HU",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Cyprus",
-          id: "CY",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Malta",
-          id: "MT",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Poland",
-          id: "PL",
-          color: "#ebdb8b",
-          customData: "2004"
-        },
-
-        {
-          title: "Romania",
-          id: "RO",
-          color: "#83c2ba",
-          customData: "2007"
-        }, {
-          title: "Bulgaria",
-          id: "BG",
-          color: "#83c2ba",
-          customData: "2007"
-        }, {
-          title: "Croatia",
-          id: "HR",
-          color: "#db8383",
-          customData: "2013"
-        }
-      ]
-    },
-	"areasSettings": {
-		"autoZoom": false,
-		// "selectedColor": "#CC0000",
-    "selectable": true
+  	"theme": "light",
+	dataProvider: {
+	  map: "worldLow",
+	  zoomLevel: 3.5,
+	  zoomLongitude: 10,
+	  zoomLatitude: 52,
+	  areas: [ {
+		  title: "Австрия",
+		  id: "AT",
+		  color: europe
+		}, {
+		  title: "Ирландия",
+		  id: "IE",
+		  color: europe
+		}, {
+		  title: "Даниия",
+		  id: "DK",
+		  color: europe
+		}, {
+		  title: "Финляндия",
+		  id: "FI",
+		  color: europe
+		}, {
+		  title: "Швеция",
+		  id: "SE",
+		  color: europe
+		}, {
+		  title: "Великобритания",
+		  id: "GB",
+		  color: europe
+		}, {
+		  title: "Италия",
+		  id: "IT",
+		  color: europe
+		}, {
+		  title: "Франция",
+		  id: "FR",
+		  color: europe
+		}, {
+		  title: "Украина",
+		  id: "UA",
+		  color: europe
+		}, {
+		  title: "Польша",
+		  id: "PL",
+		  color: europe
+		}
+	  ]
+	},
+	areasSettings: {
+		autoZoom: false,
+		selectable: true
 	}
-	// "smallMap": {}
 });
-map.zoomControl.zoomControlEnabled = false;
-map.addListener("clickMapObject", function (event) {
-    // html we would insert into bubble
-    html  = event.mapObject.customData+'===';
-    html += event.mapObject.title+'===';
-    //click position
-    html += event.event.pageX+'===';
-    html += event.event.pageY;
-    $('.mybubble').html(html);
+mapEurope.zoomControl.zoomControlEnabled = false;
+mapEurope.addListener("clickMapObject", function (event) {
+	// html we would insert into bubble
+	html  = event.mapObject.customData+'===';
+	html += event.mapObject.title+'===';
+	//click position
+	html += event.event.clientX+'===';
+	html += event.event.clientY;
+	var left = event.chart.mouseX;
+	var top = event.chart.mouseY;
+	var id = event.mapObject.id;
+	//var fieldCoords = event.currentTarget.getBoundingClientRect();
+	$('.js-mybubble').removeClass("is-active");
+	$('#'+id).css({
+		left: left,
+		top: top,
+	}).addClass("is-active");
+	$(".js-close-mybubble").on("click", function() {
+		$(".js-mybubble").removeClass("is-active");
+		return false;
+	});
 });
-var map2 = AmCharts.makeChart("chartdiv2", {
 
-  "type": "map",
-  "theme": "light",
+var CIS = "#a7c66d";
+var mapCIS = AmCharts.makeChart("chartdiv2", {
 
-    dataProvider: {
-      map: "worldLow",
-      zoomLevel: 3.5,
-      zoomLongitude: 10,
-      zoomLatitude: 52,
-      areas: [ {
-          title: "Austria",
-          id: "AT",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Ireland",
-          id: "IE",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Denmark",
-          id: "DK",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Finland",
-          id: "FI",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Sweden",
-          id: "SE",
-          color: "#67b7dc",
-          customData: "1995"
-        }, {
-          title: "Great Britain",
-          id: "GB",
-          color: "#67b7dc",
-          customData: "1973"
-        }, {
-          title: "Italy",
-          id: "IT",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "France",
-          id: "FR",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Spain",
-          id: "ES",
-          color: "#67b7dc",
-          customData: "1986"
-        }, {
-          title: "Greece",
-          id: "GR",
-          color: "#67b7dc",
-          customData: "1981"
-        }, {
-          title: "Germany",
-          id: "DE",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Belgium",
-          id: "BE",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Luxembourg",
-          id: "LU",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Netherlands",
-          id: "NL",
-          color: "#67b7dc",
-          customData: "1957"
-        }, {
-          title: "Portugal",
-          id: "PT",
-          color: "#67b7dc",
-          customData: "1986"
-        },
-
-        {
-          title: "Lithuania",
-          id: "LT",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Latvia",
-          id: "LV",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Czech Republic ",
-          id: "CZ",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Slovakia",
-          id: "SK",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Slovenia",
-          id: "SI",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Estonia",
-          id: "EE",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Hungary",
-          id: "HU",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Cyprus",
-          id: "CY",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Malta",
-          id: "MT",
-          color: "#ebdb8b",
-          customData: "2004"
-        }, {
-          title: "Poland",
-          id: "PL",
-          color: "#ebdb8b",
-          customData: "2004"
-        },
-
-        {
-          title: "Romania",
-          id: "RO",
-          color: "#83c2ba",
-          customData: "2007"
-        }, {
-          title: "Bulgaria",
-          id: "BG",
-          color: "#83c2ba",
-          customData: "2007"
-        }, {
-          title: "Croatia",
-          id: "HR",
-          color: "#db8383",
-          customData: "2013"
-        }
-      ]
-    },
-  "areasSettings": {
-    "autoZoom": false,
-    // "selectedColor": "#CC0000",
-    "selectable": true
-  }
-  // "smallMap": {}
+	"type": "map",
+  	"theme": "light",
+	dataProvider: {
+	  map: "worldLow",
+	  zoomLevel: 3.5,
+	  zoomLongitude: 10,
+	  zoomLatitude: 52,
+	  areas: [ {
+		  title: "Россия",
+		  id: "RU",
+		  color: CIS
+		}, {
+		  title: "Казахстана",
+		  id: "KZ",
+		  color: CIS
+		}, {
+		  title: "Aзербайджан",
+		  id: "AZ",
+		  color: CIS
+		}, {
+		  title: "Узбекистан",
+		  id: "UZ",
+		  color: CIS
+		}, {
+		  title: "Беларусь",
+		  id: "BY",
+		  color: CIS
+		}
+	  ]
+	},
+	areasSettings: {
+		autoZoom: false,
+		selectable: true
+	}
 });
-map2.zoomControl.zoomControlEnabled = false;
-map2.addListener("clickMapObject", function (event) {
-    // html we would insert into bubble
-    html  = event.mapObject.customData+'===';
-    html += event.mapObject.title+'===';
-    //click position
-    html += event.event.pageX+'===';
-    html += event.event.pageY;
-    $('.mybubble2').html(html);
+
+mapCIS.zoomControl.zoomControlEnabled = false;
+mapCIS.addListener("clickMapObject", function (event) {
+	// html we would insert into bubble
+	html  = event.mapObject.customData+'===';
+	html += event.mapObject.title+'===';
+	//click position
+	html += event.event.clientX+'===';
+	html += event.event.clientY;
+	var left = event.chart.mouseX;
+	var top = event.chart.mouseY;
+	var id = event.mapObject.id;
+	//var fieldCoords = event.currentTarget.getBoundingClientRect();
+	$('.js-mybubble').removeClass("is-active");
+	$('#'+id).css({
+		left: left,
+		top: top,
+	}).addClass("is-active");
+	$(".js-close-mybubble").on("click", function() {
+		$(".js-mybubble").removeClass("is-active");
+		return false;
+	});
 });
