d774a9f6831064862963a8f04b41064286f4a393
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-01-04 18:13:08 +0200

    fixes upd

--- build/js/app.js
+++ build/js/app.js
@@ -862,6 +862,15 @@ $(document).ready(function() {
 		prevArrow: '.js-tesing-prev',
 		nextArrow: '.js-tesing-next'
 	})
+	$('.js-show-password').on('mousedown', function () {
+		var input = $(this).siblings('.js-password-input');
+		input.attr("type", "text");
+		return false;
+	})
+	$('.js-show-password').on('mouseup', function () {
+		var input = $(this).siblings('.js-password-input');
+		input.attr("type", "password");
+	});
 });	
 "use strict";
 $(document).ready(function() {
