69baf830b9f3a16d5faa17ae14aa318231405b85
Eugene Godun <eugene.godun@gmail.com>
2016-01-12 16:09:10 +0700

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,49 @@
 "use strict";
 $(document).ready(function() {
+	// get scroll width;
+	var scrollWidth;
 
+	function getScrollBarWidth() {
+		var inner = document.createElement('p');
+		inner.style.width = "100%";
+		inner.style.height = "200px";
+
+		var outer = document.createElement('div');
+		outer.style.position = "absolute";
+		outer.style.top = "0px";
+		outer.style.left = "0px";
+		outer.style.visibility = "hidden";
+		outer.style.width = "200px";
+		outer.style.height = "150px";
+		outer.style.overflow = "hidden";
+		outer.appendChild(inner);
+
+		document.body.appendChild(outer);
+		var w1 = inner.offsetWidth;
+		outer.style.overflow = 'scroll';
+		var w2 = inner.offsetWidth;
+		if (w1 == w2) w2 = outer.clientWidth;
+
+		document.body.removeChild(outer);
+		if ($("html").hasClass("desktop")) {
+			scrollWidth = w1 - w2;
+		} else {
+			scrollWidth = 0;
+		}
+
+		return scrollWidth;
+
+	}
+	getScrollBarWidth();
+
+	if ($("html").hasClass("desktop")) {
+		if (body.hasClass("no-scroll")) {
+			body.css({
+				marginRight: scrollWidth
+			});
+
+		}
+	}
 	function number() { 
 		var number = $(".js-number");
 		number.each(function(){
@@ -364,5 +407,9 @@ $(document).ready(function() {
 			$( "#from" ).datepicker( "option", "maxDate", selectedDate );
 		}
 	});
+
+	$('#modal-login').on('show.bs.modal', function (e) {
+	  // do something...
+	})
 
 });
